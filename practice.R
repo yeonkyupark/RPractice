ac8a0285d792ba9b4d6158271ceb17ff24883e22
yeonkyupark <yeonkyu.park@gmail.com>
2023-01-15 00:03:27 +0000

    TIL 20230115

--- practice.R
+++ practice.R
@@ -1,3 +1,17 @@
+
+
 install.packages("tidyverse")
+install.packages(c("nycflights13", "gapminder", "Lahman"))
 library(tidyverse)
 
+# ggplot(data = <DATA>) + 
+#   <GEOM_FUNCTION>(
+#     mapping = aes(<MAPPINGS>),
+#     stat = <STAT>, 
+#     position = <POSITION>
+#   ) +
+#   <COORDINATE_FUNCTION> +
+#   <FACET_FUNCTION>
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
